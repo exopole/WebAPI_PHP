f5127fbcb8a6359babfac28d2e37edfa50ea1817
DYMEN <mendybenjamin@gmail.com>
2019-02-01 14:35:23 +0100

    correction route

--- App/Routes/Player.php
+++ App/Routes/Player.php
@@ -1,5 +1,5 @@
 <?php 
 
 $app->get('/player/{username}', \Controllers\PlayerController::class . ':getPlayer');
-$app->get('/player/{username}', \Controllers\PlayerController::class . ':getPlayerName');
+$app->get('/player/{username}/name', \Controllers\PlayerController::class . ':getPlayerName');
 $app->post('/player', \Controllers\PlayerController::class . ':createPlayer');
