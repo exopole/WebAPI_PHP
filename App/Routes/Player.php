b7c9c9f011ea705d7fb3806789853d9fde5385c8
exopole <alexandre.nicaise28@gmail.com>
2019-03-09 17:37:38 +0100

    correction

--- App/Routes/Player.php
+++ App/Routes/Player.php
@@ -1,6 +1,6 @@
 <?php 
 
-$app->post('/player/{username}/{token}', \Controllers\PlayerController::class . ':getPlayer');
+$app->post('/player/infos', \Controllers\PlayerController::class . ':getPlayer');
 //$app->get('/player/{username}/name', \Controllers\PlayerController::class . ':getPlayerName');
 $app->post('/player/create', \Controllers\PlayerController::class . ':createPlayer');
 $app->post('/player/connection', \Controllers\PlayerController::class . ':connectPlayer');
