b801562201bc4c7503f3aaa5a8c65fc77b0c60cf
exopole <alexandre.nicaise28@gmail.com>
2019-03-14 03:07:18 +0100

    test rsa

--- App/Controllers/PlayerController.php
+++ App/Controllers/PlayerController.php
@@ -171,7 +171,7 @@ class PlayerController extends Controller
 
         $keypair = sodium_crypto_box_keypair();
 
-        echo "cpicpi";
+        echo "cpicpi   ";
         //while (strpos($keypair, '#') !== false) {
           //  $keypair = sodium_crypto_box_keypair();
         
@@ -180,6 +180,7 @@ class PlayerController extends Controller
 
         $public_key = sodium_crypto_box_publickey($keypair);
         $secret_key = sodium_crypto_box_secretkey($keypair);
+        echo "   cpicpi3";
 
     	if($player && $player->player_mdp == $_POST["mdp"] ){
 
